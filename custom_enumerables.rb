03d64bb7f20ce8474860edb1e08857ece49ac8c5
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-14 23:00:31 -0400

    Add my_each_with_index

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -8,9 +8,19 @@ module Enumerable
       puts self
     end
   end
+
+  def my_each_with_index
+    if block_given?
+      for element in self do
+        yield element, self.index(element)
+      end
+    else
+      puts self
+    end
+  end    
 end
 
 p "my_each and each"
 numbers = [1, 2, 3, 4, 5]
-numbers.my_each 
-numbers.each  { |item| p item }
+numbers.my_each_with_index  { |item, index| p "item #{item} for index #{index}" }
+numbers.each_with_index  { |item, index| p "item #{item} for index #{index}" }
