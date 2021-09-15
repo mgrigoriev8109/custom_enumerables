94b54b4e400e4bdaa16378203439fbcbea3a3c46
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-14 22:54:18 -0400

    Fix #my_each with loop and self

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,10 +1,16 @@
 module Enumerable
-  def my_each(&my_block)
-    myblock.call
+  def my_each
+    if block_given?
+      for element in self do
+        yield element
+      end
+    else
+      puts self
+    end
   end
 end
 
-puts "my_each and each"
+p "my_each and each"
 numbers = [1, 2, 3, 4, 5]
-numbers.my_each  { |item| puts item }
-numbers.each  { |item| puts item }
+numbers.my_each 
+numbers.each  { |item| p item }
