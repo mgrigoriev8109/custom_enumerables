98c2bedded645f6d9ec990d9a206292d46c519b2
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-14 23:24:14 -0400

    Fix my_select with conditional new array return

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -18,9 +18,23 @@ module Enumerable
       puts self
     end
   end    
+
+  def my_select
+    new_array=[]
+    if block_given?
+      for element in self do
+        if (yield element) == true
+          new_array.push(element)
+        end
+      end
+      new_array
+    else
+      puts self
+    end
+  end    
 end
 
 p "my_each and each"
 numbers = [1, 2, 3, 4, 5]
-numbers.my_each_with_index  { |item, index| p "item #{item} for index #{index}" }
-numbers.each_with_index  { |item, index| p "item #{item} for index #{index}" }
+p numbers.my_select  { |item|  item.even? }
+p numbers.select  { |item|  item.even? }
