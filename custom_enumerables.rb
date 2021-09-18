7009ba048ad0e0a443f9540d1c8c74d0e47e211f
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-18 06:16:31 -0400

    Add tests for each method

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -120,8 +120,48 @@ def multiply_els(array)
 end
 
 map_proc = Proc.new  { |i| i*i }
+numbers = [1, 2, 3, 4, 5]
+words = ['cat', 'dog', 'banana']
 
-words = [2, 4, 5]
-p "test1"
-p words.my_map(map_proc)
-p words.map { |i| i*i } 
+puts "#my_each vs each"
+numbers.my_each { |item| puts item }
+numbers.each { |item| puts item }
+
+puts "#my_each_with_index vs each_with_index"
+numbers.my_each_with_index { |item, index| puts "item: #{item}, index: #{index}" }
+numbers.each_with_index { |item, index| puts "item: #{item}, index: #{index}" }
+
+p "#my_select vs select"
+p numbers.my_select { |num|  num.even?  } 
+p numbers.select { |num|  num.even?  } 
+
+p "#my_all? vs all?"
+p numbers.my_all? { |num|  num.even?  } 
+p numbers.all? { |num|  num.even?  } 
+
+p "#my_any? vs any?"
+p numbers.my_any? { |num|  num.even?  } 
+p numbers.any? { |num|  num.even?  } 
+
+p "#my_none? vs none?"
+p numbers.my_none? { |num|  num.even?  } 
+p numbers.none? { |num|  num.even?  } 
+
+p "#my_count vs count"
+p numbers.my_count  { |num| num > 1 }
+p numbers.count { |num| num > 1 }
+
+p "#my_map vs map proc"
+p numbers.my_map(map_proc)
+p numbers.map { |num| num*num }
+
+p "my_map vs map block"
+p numbers.my_map { |num| num*num }
+p numbers.map { |num| num*num }
+
+p "#my_inject vs inject"
+p numbers.my_inject { |sum, number| sum + number } 
+p numbers.inject { |sum, number| sum + number } 
+
+p "#multiply_els"
+p multiply_els(numbers)
